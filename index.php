beb90e847176174aa277a8dd2e28a6e053c279e3
ginoskurtloven <ginoskurtloven@gmail.com>
2016-07-18 16:16:13 +0800

    Created Initial Repository

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<title>Portal \ Database System</title>
+		<!-- Skeleton Framework Style -->
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" href="css/skeleton.css">
+
+		<!-- Style -->
+		<link rel="css/stylesheet" href="css/">
+
+	</head>
+	<body style="background-color: #FDFCFA">
+		<h1>Portal | Database System</h1>
+		<p>No Sytem is safe! Stay Anonymouse Online</p>
+		<form action="" method="POST">
+			<div class="row">
+			<div class="six columns">
+				<!-- <label for="">Username</label> -->
+				<input class="u-full-width" type="text" name="" id="" placeholder="Username">
+			</div>
+			</div> <!-- end of row -->
+
+			<div class="row">
+			<div class="six columns">
+				<!-- <label for="">Username</label> -->
+				<input class="u-full-width" type="password" name="" id="" placeholder="Password">
+			</div>
+			</div> <!-- end of row -->
+			<input type="submit" value="Login">
+		</form>
+		<script type="text/javascript">
+			alert("Warning You Are Entering A complete Legal Database");
+		</script>
+	</body>
+</html>
