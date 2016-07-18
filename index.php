11e18ed360ec9d641ae3c7a651d9404d9547a149
ginoskurtloven <ginoskurtloven@gmail.com>
2016-07-18 22:26:01 +0800

    Create and Build the Initial Directory Structure

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE>
-<html>
+<html lang="en">
 	<head>
-		<title>Portal \ Database System</title>
+		<title>Portal \ Info Database System</title>
 		<!-- Skeleton Framework Style -->
 		<link rel="stylesheet" href="css/normalize.css">
 		<link rel="stylesheet" href="css/skeleton.css">
@@ -11,24 +11,28 @@
 
 	</head>
 	<body style="background-color: #FDFCFA">
+		<div class="container">
 		<h1>Portal | Database System</h1>
 		<p>No Sytem is safe! Stay Anonymouse Online</p>
-		<form action="" method="POST">
-			<div class="row">
-			<div class="six columns">
-				<!-- <label for="">Username</label> -->
-				<input class="u-full-width" type="text" name="" id="" placeholder="Username">
-			</div>
-			</div> <!-- end of row -->
 
-			<div class="row">
-			<div class="six columns">
-				<!-- <label for="">Username</label> -->
-				<input class="u-full-width" type="password" name="" id="" placeholder="Password">
-			</div>
-			</div> <!-- end of row -->
-			<input type="submit" value="Login">
-		</form>
+			<form action="" method="POST">
+				<div class="row">
+				<div class="six columns">
+					<!-- <label for="">Username</label> -->
+					<input class="u-full-width" type="text" name="" id="" placeholder="Username">
+				</div>
+				</div> <!-- end of row -->
+
+				<div class="row">
+				<div class="six columns">
+					<!-- <label for="">Username</label> -->
+					<input class="u-full-width" type="password" name="" id="" placeholder="Password">
+				</div>
+				</div> <!-- end of row -->
+				<input type="submit" value="Login">
+			</form>
+			<p id="notification" style="display: none;">Record Not Found</p>
+		</div> <!-- end of container -->
 		<script type="text/javascript">
 			alert("Warning You Are Entering A complete Legal Database");
 		</script>
